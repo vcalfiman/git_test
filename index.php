bc6c995e30ce56cd0ee5f1241403cdfdc73176b5
Victor Calfiman <40865225+vcalfiman@users.noreply.github.com>
2021-10-23 13:52:25 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+<h1>Hola Victor este es tu Git de Prueba</h1>
+<p>Probando Git de forma basica para luego subirlo a GitHub</p>
+<p>Agregando un poco mas de datos antes de subir a github</p>
+<p>Agregando contenido de una nueva rama</p>
+
+
+
+?>
